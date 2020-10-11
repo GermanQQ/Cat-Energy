611fc5c5227a964a17d095655dfc6d35ad4f589a
German <naushnik.good@gmail.com>
2020-10-11 23:41:30 +0300

    Поправил бургер

--- js/script.js
+++ js/script.js
@@ -11,17 +11,20 @@ $(document).ready(function(){
         infinite: false
     });
 
+
     let nav = $('.main-nav__wrapper'),
-	scrollPrev = 0;
+    scrollPrev = 0;
 
 $(window).scroll(function() {
 	let scrolled = $(window).scrollTop();
  
 	if ( scrolled > 100 && scrolled > scrollPrev ) {
-		nav.removeClass('main-nav__wrapper--active');
+        nav.removeClass('main-nav__wrapper--active'),
+        $('.main-nav__burger').removeClass('main-nav__burger--active')
 	} 
 	scrollPrev = scrolled;
 });
+
 });
 
 
