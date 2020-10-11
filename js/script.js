d35ce85aa9cbf4cc074604f99fd896cb6520ee64
German <naushnik.good@gmail.com>
2020-10-11 19:16:52 +0300

    Слайдер добавил

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$('.main-nav__burger').on('click', function(e){
-    e.preventDefault;
-    $(this).toggleClass('main-nav__burger--active');
-    $('.main-nav__wrapper').toggleClass('main-nav__wrapper--active');
+$(document).ready(function(){
+
+    $('.main-nav__burger').on('click', function(e){
+        e.preventDefault;
+        $(this).toggleClass('main-nav__burger--active');
+        $('.main-nav__wrapper').toggleClass('main-nav__wrapper--active');
+    });
+
+    $('.before-after__slider-block').slick({ 
+        dots: true,
+        infinite: false
+    });
 });
+
+
