12a019f975f89dd68b4722daf59afcd3005d1142
German <naushnik.good@gmail.com>
2020-10-11 19:58:23 +0300

    Убрать менюпри скроле вниз

--- js/script.js
+++ js/script.js
@@ -10,6 +10,18 @@ $(document).ready(function(){
         dots: true,
         infinite: false
     });
+
+    let nav = $('.main-nav__wrapper'),
+	scrollPrev = 0;
+
+$(window).scroll(function() {
+	let scrolled = $(window).scrollTop();
+ 
+	if ( scrolled > 100 && scrolled > scrollPrev ) {
+		nav.removeClass('main-nav__wrapper--active');
+	} 
+	scrollPrev = scrolled;
+});
 });
 
 
